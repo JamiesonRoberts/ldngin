281aff65e3a7102efbd6cfff10f78f82dc9a8302
Jamieson Roberts <hello@jamiesonroberts.com>
2019-04-15 08:31:58 -0400

    Notification emails

--- resources/views/emails/notify.blade.php
+++ resources/views/emails/notify.blade.php
@@ -1,1 +1,12 @@
 <h1>New subscription</h1>
+<p><strong>Name:</strong> {{ $submission->name }}</p>
+<p><strong>Email:</strong> {{ $submission->email }}</p>
+@if($submission->consentNewsletter)
+    <p><strong>Meet-up Newsletter Consent:</strong> Yes</p>
+@endif
+
+@if($submission->consentAvailability)
+    <p><strong>Gin Availability Newsletter Consent:</strong> Yes</p>
+@else
+    <p><strong>Gin Availability Newsletter Consent:</strong> No</p>
+@endif
---
+++ resources/views/emails/thankYou.blade.php
@@ -0,0 +1,2 @@
+<h1>Thank you for subscribing!</h1>
+<p>Thank you for subscribing, we will be in touch soon!</p>
