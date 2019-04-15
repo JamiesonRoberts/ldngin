a26cc199c3ea0a6ebbef77534d8f401b71dea5b6
Jamieson Roberts <hello@jamiesonroberts.com>
2019-04-15 00:22:35 -0400

    Recapta validation

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -11,20 +11,20 @@ class MemberController extends Controller
     //
     public function __invoke(FormSubmission $request)
     {
-//        $client = new Client();
-//        $response = $client->post('https://www.google.com/recaptcha/api/siteverify',
-//            [
-//                'form_params' => [
-//                    'secret' => env('RECAPTCHA_SECRET_KEY', false),
-//                    'response' => $request->get('g-recaptcha-response')
-//                ]
-//            ]
-//        );
-//3
-//        $body = json_decode((string) $response->getBody());
-//        if (!$body->success) {
-//            return redirect()->back()->withInput();
-//        };
+        $client = new Client();
+        $response = $client->post('https://www.google.com/recaptcha/api/siteverify',
+            [
+                'form_params' => [
+                    'secret' => env('RECAPTCHA_SECRET_KEY', false),
+                    'response' => $request->get('g-recaptcha-response')
+                ]
+            ]
+        );
+3
+        $body = json_decode((string) $response->getBody());
+        if (!$body->success) {
+            return redirect()->back()->withInput();
+        };
 
         dump('form controller');
         dd($request->all());
